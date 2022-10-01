975782f77448780d9b6ef539dbfccadc1743b46f
xanthoko <konxantho@gmail.com>
2022-10-01 11:31:19 +0300

    Separate boost methods

--- boost.py
+++ boost.py
@@ -13,43 +13,56 @@ from loader import output_chargers
 from loader import load_previous_chargers
 
 
+def get_redundant_chargers(chargers: tuple[int, int],
+                           supply_used: float,
+                           previous_chargers: tuple[int, int]) -> tuple[int, int]:
+    scs, fcs = chargers
+    available_supply = SLOW_CHARGE_CAP * scs + FAST_CHARGE_CAP * fcs
+    diff = available_supply - supply_used
+
+    # the diff has to be greater than 200 to be able to remove at
+    # least a charger
+    if diff < 200:
+        return 0, 0
+
+    # check how many fcs can be removed
+    removable_fcs = fcs - previous_chargers[1]
+    unused_fcs = diff // 400
+    removed_fcs = min(removable_fcs, unused_fcs)
+
+    diff -= removed_fcs * 400
+
+    # check how many scs can be removed
+    removable_scs = scs - previous_chargers[0]
+    unused_scs = diff // 200
+    removed_scs = min(removable_scs, unused_scs)
+
+    return removed_scs, removed_fcs
+
+
 def remove_excess_supply(chargers: Genome, previous_chargers: Genome,
                          ds: np.ndarray) -> None:
     total_removed_scs = 0
     total_removed_fcs = 0
     supplies_used = np.sum(ds, axis=0)
 
-    for i, (scs, fcs) in chargers.items():
-        available_supply = SLOW_CHARGE_CAP * scs + FAST_CHARGE_CAP * fcs
-        diff = available_supply - supplies_used[i]
-
-        # the diff has to be greater than 200 to be able to remove at
-        # least a charger
-        if diff < 200:
-            continue
-
-        # check how many fcs can be removed
-        removable_fcs = fcs - previous_chargers[i][1]
-        unused_fcs = diff // 400
-        removed_fcs = min(removable_fcs, unused_fcs)
-
-        diff -= removed_fcs * 400
-
-        # check how many scs can be removed
-        removable_scs = scs - previous_chargers[i][0]
-        unused_scs = diff // 200
-        removed_scs = min(removable_scs, unused_scs)
+    for i, charger in chargers.items():
+        removed_scs, removed_fcs = get_redundant_chargers(
+            charger,
+            supplies_used[i],
+            previous_chargers[i]
+        )
 
         total_removed_scs += removed_scs
         total_removed_fcs += removed_fcs
 
         # update genome
-        chargers[i] = (scs - removed_scs, fcs - removed_fcs)
+        chargers[i] = (charger[0] - removed_scs, charger[1] - removed_fcs)
 
     saved_cost = (total_removed_scs + 1.5 * total_removed_fcs) * 600
     print('[INFO] - Excess supply removed')
     print(f'{total_removed_scs} scs removed')
-    print(f'{total_removed_fcs} scs removed')
+    print(f'{total_removed_fcs} fcs removed')
     print(f'{saved_cost} cost saved')
 
 
